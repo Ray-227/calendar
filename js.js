50bdef767d9b03f09b3d4738e08fd52839a022fd
Ray <ray-227@ya.ru>
2020-11-14 03:16:10 +0500

    Сделал функционал переключения календаря, переписал алгоритм при обновлении чисел, для примера создал old_js.

--- js.js
+++ js.js
@@ -1,275 +1,307 @@
-"use strict"
-
 /*
-  TODO: Сделать вывод название месяца и года (ГОТОВО)
-  TODO: Сделать вывод дней недели (ГОТОВО)
-  TODO: Сделать вывод всех чисел в месяцев (ГОТОВО)
-  TODO: Сделать стрелки вправо и влево для переключения месяца и года
-
-  * CLASESS:
+  * CLASSES
   * calendar-container
-  * calendar-container__header
-  * calendar-container__day
-  * calendar-container__date
-  * calendar-container__clear
-  * calendar-container__accept
-  
-  ? new Date(year, month, date)
-  */
 
-  class Calendar {
+    * calendar-container__header
+      * calendar-container__title
+      * calendar-container__prev
+      * calendar-container__next
 
-    constructor(year = "currentYear", month = "currentMonth", date = "currentDate") {
-      this.date = new Date();
-      this.getFullDate(year, month, date);
-      this.drawCalendar();
-    }
+    * calendar-container__day
 
-    getFullDate(year, month, date) {
-      if (year === "currentYear") {
-        this.currentFullYear = this.date.getFullYear();
-      } else if (typeof(year) === "number") {
-        this.date.setFullYear(year);
-        this.currentFullYear = this.date.getFullYear();
-      } else {
-        this.drawTag("Error Year: Неправильно введён год", ".calendar-container", "calendar-container__header");
-        return {"Error Year": "Неправильно введён год"};
+    * calendar-container__date
+
+    * calendar-container__buttons
+      * calendar-container__clear
+      * calendar-container__accept
+
+    * calendar_error
+*/
+
+class Calendar {
+  constructor() {
+    this.fullDate = new Date(); // Получаем текущию дату.
+    // Получаем текущий год, месяц, число.
+    this.year = this.fullDate.getFullYear();
+    this.month = this.fullDate.getMonth();
+    this.date = this.fullDate.getDate();
+    // Определяем на какой день недели попадает первое число текущего месяца.
+    this.firstDay = () => {
+      this.fullDate.setDate(1);
+      let firstDay = this.fullDate.getDay( this.fullDate.getDate() );
+      if (firstDay === 0) {
+        firstDay = 7;
       }
+      this.fullDate.setDate(this.date);
+      return firstDay;
+    };
 
-      if (month === "currentMonth") {
-        this.currentMonth = this.date.getMonth();
-      } else if (typeof(month) === "number") {
-        this.date.setMonth(month);
-        this.currentMonth = this.date.getMonth();
-      } else {
-        this.drawTag("Error Month: Неправильно введён месяц", ".calendar-container", "calendar-container__header");
-        return {"Error Month": "Неправильно введён месяц"};
-      }
+    /*
+      Почему если я хочу сделать constructor класса со следующим кодом, this.fullDate в методах неопределенна, словно я её и не создавал?
 
-      if (date === "currentDate") {
-        this.currentDate = this.date.getDate();
-      } else if (typeof(date) === "number") {
-        this.date.setDate(date);
-        this.currentDate = this.date.getDate();
-      } else {
-        this.drawTag("Error Date: Неправильно введено число", ".calendar-container", "calendar-container__header");
-        return {"Error Date": "Неправильно введено число"};
-      }
-    }
+      this.fullDate = new Date();
 
-    drawCalendar() {
-      this.drawTag("none", ".calendar-container", "calendar-container__header");
-      this.drawTag("none", ".calendar-container__header", "calendar-container__title", "h1");
-      this.drawTag(`${this.getNameMonth(this.currentMonth)} ${this.currentFullYear}`, ".calendar-container__title", "none", "text");
-
-      this.drawTag("<", ".calendar-container__header", "calendar-container__prev", "div", "start");
-      this.drawTag(">", ".calendar-container__header", "calendar-container__next");
-      
-      // Выводим название недель.
-      for (let i = 1; i <= 7; i++) {
-        this.drawTag(this.getNameDay(i), ".calendar-container", ".calendar-container__day");
-      }
-
-      let ValuesForCalendarTable, startTable, countDate;
-      ValuesForCalendarTable = this.getValuesForCalendarTable();
-      startTable = ValuesForCalendarTable.startTable;
-      countDate = ValuesForCalendarTable.countDate;
-
-      for (;startTable <= countDate; startTable++) {
-        // Изменяя число *Date, меняется и месяц *Month.
-        this.date.setMonth(this.currentMonth); // ? Можно сделать условие и устанавливать текущий месяц, если startTable < 0 или startTable > lastDateCurrentMonth
-        this.date.setDate(startTable);
-        this.drawTag(this.date.getDate(), ".calendar-container", "calendar-container__date");
-      }
-      
-      this.drawTag("none", ".calendar-container", "calendar-container__buttons");
-      this.drawTag("Очистить", ".calendar-container__buttons", "calendar-container__clear", "button");
-      this.drawTag("Принять", ".calendar-container__buttons", "calendar-container__accept", "button");
-    }
-
-    drawTag(data, to, className = "none", element = "div", where = "end") {
-      let block, tag;
-      block = document.querySelector( String(to).trim() );
-      
-      if(element !== "text") {
-        tag = document.createElement(element);
-      } else if (element === "text") {
-        tag = document.createTextNode(data);
-      }
-
-      if(className !== "none"){
-        tag.className = String(className).trim();
-      }
-
-      if(data !== "none"){
-        tag.innerHTML = data;
-      }
-
-      if (where === "start") {
-        block.prepend(tag);
-      } else if (where === "end") {
-        block.append(tag);
-      }
-    }
-
-    updateСalendar(year = "currentYear", month = "currentMonth", date = "currentDate") {
-      this.getFullDate(year, month, date);
-      document.querySelector(".calendar-container__title").innerHTML = `${this.getNameMonth(this.currentMonth)} ${this.currentFullYear}`;
-
-      let dates, datesLength;
-      dates = document.querySelectorAll(".calendar-container__date");
-      datesLength = dates.length;
-      
-      // for (let i = 0; i < datesLength; i++) {
-      //   this.date.setMonth(this.currentMonth);
-      //   dates[i].innerHTML = 
-      // }
-      
-    }
-
-    getValuesForCalendarTable() {
-      /*
-        TODO: Узнать в какой день недели начало текущего месяца (Готово)
-        TODO: Узнать сколько нужно вывести дней прошлого месяца (Готово)
-      */
-
-      /*
-        Пример:
-          Пусть текущая дата будет 01.11.2020 (1 Ноября 2020 года).
-
-          Работа алгоритма:
-            firstDayCurrentMonth = 7 (1-ое ноября 2020 года это воскресенье, 7-ой день недели, в объекте Date это 0, поэтому смело присваиваем 7)
-            countDatePastMonthInStartCurrentMonth = 7 - 1 (6);
-              Если countDatePastMonthInStartCurrentMonth больше 0, тогда 
-              countDatePastMonthInStartCurrentMonth = -( countDatePastMonthInStartCurrentMonth(6) - 1 ); -1 ибо есть еще 0, и делаем все это отрицательным числом.
-      */
-
-      let countDate, firstDayCurrentMonth, countDatePastMonthInStartCurrentMonth, startTable;
-      countDate = 35; // Количество чисел для вывода календаря
-
-      /* 
-        * Первый день недели в текущем месяце.
-        Зная в какой день недели начинается месяц, мы можем определить,
-        сколько дней прошлого месяца нужно вывести.
-      */
-      this.date.setDate(1); // Жестко переводим текущий месяц на 1-ое число.
-      firstDayCurrentMonth = Number( this.date.getDay( this.date.getDate() ) ); // Получаем число, обозначающее день недели, 1-го числа текущего месяца.
-      // Если 1-ое число месяца падает на воскресенье (это 0), присваиваем 7.
-      if (firstDayCurrentMonth === 0) {
-        firstDayCurrentMonth = 7;
-      }
-      this.date.setDate(this.currentDate); // Переводим текущий месяц на текущее число.
-
-      countDatePastMonthInStartCurrentMonth = firstDayCurrentMonth - 1; // Количество дней прошлого месяца в начале текущего месяца. Делаем -1, чтобы вывести все дни недели, кроме текущей.
-      if (countDatePastMonthInStartCurrentMonth > 0) {
-        countDatePastMonthInStartCurrentMonth = -(countDatePastMonthInStartCurrentMonth - 1); // Делаем -1 ибо есть еще 0, который являеться последним числом прошлого месяца.
-        if (countDatePastMonthInStartCurrentMonth === -0) {
-          countDatePastMonthInStartCurrentMonth = 0;
-        }
-      }
-
-
-      startTable = countDatePastMonthInStartCurrentMonth; // Счетчик может быть отрицательным либо 0, если 0, тогда увеличим его на 1, ибо getDate(0) это последний день прошлого месяца.
-      countDate += countDatePastMonthInStartCurrentMonth;
-      // Если 1-ое число месяца Пн и кол-во дней прош. мес. в нач. текущего 0, увеличиваем счетчик, чтобы вывести 35 чисел, иначе уменьшаем кол-во дней до 34 ибо есть еще 0, который выводит последний день прошлого месяца.
-      if (firstDayCurrentMonth === 1 && countDatePastMonthInStartCurrentMonth === 0) {
-        startTable++;
-      } else {
-        countDate--;
-      }
-
-      return {startTable: startTable, countDate: countDate};
-    }
-
-    getNameDay(day) {
-      let dayNumber = Number(day);
-
-      switch (dayNumber) {
-        case 7:
-          return "Вс";
-        break;
-        case 1:
-          return "Пн";
-        break;
-        case 2:
-          return "Вт";
-        break;
-        case 3:
-          return "Ср";
-        break;
-        case 4:
-          return "Чт";
-        break;
-        case 5:
-          return "Пт";
-        break;
-        case 6:
-          return "Сб";
-        break;
-      }
-
-    }
-
-    getNameMonth(month) {
-      let monthNumber = Number(month);
-    
-      switch (monthNumber) {
-        case 0: 
-          return "Январь";
-        break;
-        case 1: 
-          return "Февраль";
-        break;
-        case 2: 
-          return "Март";
-        break;
-        case 3: 
-          return "Апрель";
-        break;
-        case 4: 
-          return "Май";
-        break;
-        case 5: 
-          return "Июнь";
-        break;
-        case 6: 
-          return "Июль";
-        break;
-        case 7: 
-          return "Август";
-        break;
-        case 8: 
-          return "Сентябрь";
-        break;
-        case 9: 
-          return "Октябрь";
-        break;
-        case 10: 
-          return "Ноябрь";
-        break;
-        case 11: 
-          return "Декабрь";
-        break;
-      }
-      
-    }
-
+      this.calendar = {
+        year: this.fullDate.getFullYear(),
+        month: this.fullDate.getMonth(),
+        date: this.fullDate.getDate(),
+        firstDay: () => {
+          this.fullDate.setDate(1);
+          let firstDay = this.fullDate.getDay( this.fullDate.getDate() );
+          if (firstDay === 0) {
+            firstDay = 7;
+          }
+          this.fullDate.setDate(this.date);
+          return firstDay;
+            }
+      };
+    */
   }
 
-window.onload = () => {
+  draw() {
+    drawTag('.calendar-container', 'none', 'calendar-container__header');
+    drawTag('.calendar-container__header', `${getNameMonth(this.month)} ${this.year}`, 'calendar-container__title');
+    drawTag('.calendar-container__header', '<', 'calendar-container__prev', 'div', 'start');
+    drawTag('.calendar-container__header', '>', 'calendar-container__next');
+
+    // Выводим название недель, знаю можно сделать проще и выводить просто название недель из массива.
+    for (let i = 1; i <= 7; i++) {
+      drawTag('.calendar-container', getNameDay(i), '.calendar-container__day');
+    }
+
+    this.drawDates();
+
+    drawTag('.calendar-container', 'none', 'calendar-container__buttons');
+    drawTag('.calendar-container__buttons', 'Очистить', 'calendar-container__clear', 'input button');
+    drawTag('.calendar-container__buttons', 'Принять', 'calendar-container__accept', 'input button');
+  }
+
+  update() {
+    document.querySelector('.calendar-container__title').innerHTML = `${getNameMonth(this.month)} ${this.year}`;
+
+    let dates, datesLength;
+    dates = document.querySelectorAll('.calendar-container__date');
+    datesLength = dates.length;
+
+    this.drawDates(dates, datesLength);
+  }
+
+  drawDates(dates = false, datesLength = false) {
+    /* 
+      Создаю новую Date, ранее я использовал только одну, 
+      определенную в конструкторе, из-за этого у меня были баги,
+      первый баг был связан с тем, что после Мая переставали переключаться месяца назад, 
+      но вперед переключалось нормально, второй баг связан, с тем, что с Января перескакивало на Март,
+      пропуская Февраль. Код с багом есть на github.
+
+      Я пытался понять суть бага и с чем он связан и предположил, что у него две причины:
+        1) Я использовал одну new Date, из-за этого все перемешивалось в кашу;
+        2) Я не делал setMonth(this.month) на последней итераций цикла, данная операция у меня была перед setDate, 
+        логичнее её расположить после изменения Date.
+    */
+    let tempDate = new Date(this.year, this.month);
+
+    let startTable, countDate;
+
+    startTable = -(this.firstDay() - 1); // Делаем отрицательным, чтобы выводить дни прошлого месяца.
+    countDate = 34;
+
+    /*
+      Пример работы условия:
+        Пусть startTable = -5, тогда выполняется else,
+        где мы делаем startTable = -4, ибо есть еще 0,
+        а countDate = 34 + (-4), countDate = 30.
+        Итого у нас ровно 35 итераций цикла, не забывайте про 0.
+    */
+    // Если -0, значит у нас нету чисел с прошлого месяца.
+    if (startTable === -0) {
+      startTable = 1;
+      countDate = 35;
+    } else {
+      startTable += 1; // Делаем +1, но т.к число отрицательное это уменьшает его, не забываем у нас есть еще 0, он выводит последнее число прошлого месяца.
+      countDate += startTable; // Чтобы не выходить за границу 35 чисел для вывода в календаре.
+    }
+    // Если элементов с числами месяца нет, тогда выводим месяц создавая элементы.
+    if ( document.querySelector('.calendar-container__date') ) {
+      if (!dates) {
+        console.log('Error dates: неизвестно куда выводить новые числа');
+        console.log('Error datesLength: неизвестно количество элементов');
+        return 0;
+      }
+
+      let i = 0;
+      for (; startTable <= countDate; startTable++) {
+        // По сути ненужная проверка, но пусть будет, так спокойнее.
+        if (i < datesLength) {
+          tempDate.setMonth(this.month); // Не понимаю, почему если убрать эту строчку кода, февраль setDate(0) выводит 29, а не 31. В других месяцах ошибок в числах не заметил.
+          tempDate.setDate(startTable);
+          console.log(getNameMonth(this.month), tempDate.getDate(), startTable);
+          dates[i].innerHTML = tempDate.getDate();
+          tempDate.setMonth(this.month);
+          i++;
+        }
+      }
+    } else {
+      for (; startTable <= countDate; startTable++) {
+        tempDate.setMonth(this.month);
+        tempDate.setDate(startTable);
+        drawTag('.calendar-container', tempDate.getDate(), 'calendar-container__date');
+      }
+    }
+  }
+
+  prev() {
+    if (this.month === 0) {
+      this.fullDate.setFullYear(this.year - 1);
+      this.year = this.fullDate.getFullYear();
+      this.fullDate.setMonth(11);
+      this.month = this.fullDate.getMonth();
+    } else {
+      this.fullDate.setMonth(this.month - 1);
+      this.month = this.fullDate.getMonth();
+    }
+
+    this.update();
+  }
+
+  next() {
+    if (this.month === 11) {
+      this.fullDate.setFullYear(this.year + 1);
+      this.year = this.fullDate.getFullYear();
+      this.fullDate.setMonth(0);
+      this.month = this.fullDate.getMonth();
+    } else {
+      this.fullDate.setMonth(this.month + 1);
+      this.month = this.fullDate.getMonth();
+    }
+
+    this.update();
+  }
+}
+
+
+let calendar = new Calendar();
+calendar.draw();
+
+document.querySelector('.calendar-container__prev').addEventListener('click', function() {
+  calendar.prev();
+});
+
+document.querySelector('.calendar-container__next').addEventListener('click', function() {
+  calendar.next();
+});
+
+
+function drawTag(to, data = 'none', className = 'none', element = 'div', where = 'end') { 
   /*
-    Вывод месяца, возможен ввод трех необязательных параметров, если оставить параметр пустым, тогда он примет текущий год/месяц/число.
-    new Calendar(?year:number, ?month:number, ?date:number)
+  Данная функция позволяет облегчить создание новых html тегов и улучшить читаемость кода.
 
-    let calendar = new Calendar(); - выведет текущий год, месяц и число.
-    let calendar = new Calendar(2025, 0, 1); - выведет 1 Января 2025 года.
+  Обозначение параметров, передавайте параметры в нижнем регистре:
+    1) to - определяет куда вставить создаваемый элемент (далее new elem) (обязательный!);
+    2) data - информация которая поместиться в new elem;
+    3) className - class который присвоиться new elem;
+    4) element - название тега new elem, по умолчанию div, 
+      можно передать input с любым атрибутом type, date станет атрибутом value, 
+      (пример: 'input button', следуйте данному правилу, иначе будет оишбка);
+    5) where - куда вставить new elem, в начало to или в конец, по умолчанию end (может принимать только start и end).
+*/
+  let block, tag;
 
-    Если желаете вывести текущий год, введеный месяц и текущее число.
-    let calendar = new Calendar("currentYear", 2, "currentDate"); - выведет 1 Января 2025 года.
-  */
+  block = document.querySelector( String(to).trim() );
+
+  if (element.toLowerCase().includes('input')) {
+    element = element.split(' ');
+    tag = document.createElement(element[0]);
+    tag.type = element[1];
+    tag.value = data;
+  } else {
+    tag = document.createElement(element);
+  }
+
+  if(className.toLowerCase() !== 'none'){
+    tag.className = String(className).trim();
+  }
+
+  if(data !== 'none' && !Array.isArray(element)){
+    tag.innerHTML = data;
+  }
+
+  if (where.toLowerCase() === 'start') {
+    block.prepend(tag);
+  } else if (where.toLowerCase() === 'end') {
+    block.append(tag);
+  }
+}
+
+function getNameMonth(month) {
+  let monthNumber = Number(month);
+
+  switch (monthNumber) {
+    case 0: 
+      return 'Январь';
+    break;
+    case 1: 
+      return 'Февраль';
+    break;
+    case 2: 
+      return 'Март';
+    break;
+    case 3: 
+      return 'Апрель';
+    break;
+    case 4: 
+      return 'Май';
+    break;
+    case 5: 
+      return 'Июнь';
+    break;
+    case 6: 
+      return 'Июль';
+    break;
+    case 7: 
+      return 'Август';
+    break;
+    case 8: 
+      return 'Сентябрь';
+    break;
+    case 9: 
+      return 'Октябрь';
+    break;
+    case 10: 
+      return 'Ноябрь';
+    break;
+    case 11: 
+      return 'Декабрь';
+    break;
+  }
   
 }
-let calendar = new Calendar();
 
+function getNameDay(day) {
+  let dayNumber = Number(day);
 
+  switch (dayNumber) {
+    case 7:
+      return 'Вс';
+    break;
+    case 1:
+      return 'Пн';
+    break;
+    case 2:
+      return 'Вт';
+    break;
+    case 3:
+      return 'Ср';
+    break;
+    case 4:
+      return 'Чт';
+    break;
+    case 5:
+      return 'Пт';
+    break;
+    case 6:
+      return 'Сб';
+    break;
+  }
 
+}
